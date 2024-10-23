0559535572e8f2a962fa95a63ed56dd6cdd99f14
Vina Quiambao <mariavinaq@gmail.com>
2024-10-23 00:30:50 -0400

    Add navigation to header logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,17 @@
-
+import { useNavigate } from 'react-router-dom';
 import code from '../../assets/images/code.png';
 import bookmark from '../../assets/images/bookmark.png';
 import avatar from '../../assets/images/avatar.png';
 import './Header.scss';
 
 const Header: React.FC = () => {
+    const navigate = useNavigate();
+    const handleOnClick = () => {
+        navigate('/');
+    }
     return (
         <header className='header'>
-            <div className='header__logo-container'>
+            <div className='header__logo-container' onClick={handleOnClick}>
                 <img className='header__logo-icon' src={code} alt='site logo' />
                 <p className='header__logo-title'>c o d e x</p>
             </div>
