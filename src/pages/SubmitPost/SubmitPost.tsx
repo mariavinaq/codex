c3ac95a45581441e2693abe98760ddc4a87170ac
Vina Quiambao <mariavinaq@gmail.com>
2025-02-21 00:39:47 -0500

    Fix button styling

--- src/pages/SubmitPost/SubmitPost.tsx
+++ src/pages/SubmitPost/SubmitPost.tsx
@@ -128,14 +128,14 @@ const SubmitPost = () => {
                             <input className='submit-post__file-input' type='file' id='file' name='file' required></input>
                         </div>
                         <div className='submit-post__actions'>
-                            <button className='submit-post__post-button' type='reset' onClick={handleReset}>Reset</button>
-                            <button className='submit-post__post-button' type='submit' disabled={posting}>{posting ? 'Posting...' : 'Post'}</button>
+                            <button className='submit-post__button' type='reset' onClick={handleReset}>Reset</button>
+                            <button className='submit-post__button' type='submit' disabled={posting}>{posting ? 'Posting...' : 'Post'}</button>
                         </div>
                     </form>
                     <form className='submit-post__prompter' onSubmit={(event) => handleOnSubmitPrompt(event)}>
                         <label className='submit-post__form-title submit-post__form-title--prompter' htmlFor='prompt'>Generate code with AI</label>
                         <input className='submit-post__form-input submit-post__form-input--prompter' id='prompt' name='prompt' value={prompt} onChange={(event) => setPrompt(event.target.value)}></input>
-                        <button className={`submit-post__post-button submit-post__post-button--prompter submit-post__post-button--prompter${fetchingAi ? '-loading' : ''}`} type='submit' disabled={fetchingAi}>{fetchingAi ? 'Wait for it...' : 'Go'}</button>
+                        <button className={`submit-post__prompter-button ${fetchingAi ? 'submit-post__prompter-button--loading' : ''}`} type='submit' disabled={fetchingAi}>{fetchingAi ? 'Wait for it...' : 'Go'}</button>
                     </form>
                 </div>
             </div>
